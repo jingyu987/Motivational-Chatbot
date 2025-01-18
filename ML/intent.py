63adafc8a9a4a74d805c22f890174acfc0285102
Tim Lim <tim.lim@bytedance.com>
2025-01-18 21:16:54 +0800

    Not training model alr

--- ML/intent.py
+++ ML/intent.py
@@ -1,51 +1,41 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
-from datasets import Dataset
+# from transformers import RobertaTokenizerFast, RobertaForSequenceClassification, TextClassificationPipeline
 
-import requests
-from huggingface_hub import configure_http_backend
 
-import urllib3
-urllib3.disable_warnings()
+# import requests
+# from huggingface_hub import configure_http_backend
 
-def backend_factory() -> requests.Session:
-    session = requests.Session()
-    session.verify = False
-    return session
+# import urllib3
+# urllib3.disable_warnings()
 
-configure_http_backend(backend_factory=backend_factory)
+# def backend_factory() -> requests.Session:
+#     session = requests.Session()
+#     session.verify = False
+#     return session
 
-# Prepare dataset
-data = {"text": ["What's the weather like?", "Book a flight for tomorrow."],
-        "labels": [0, 1]}  # 0 = "Check Weather", 1 = "Book Flight"
-dataset = Dataset.from_dict(data)
+# configure_http_backend(backend_factory=backend_factory)
 
-# Load pre-trained model and tokenizer
-model_name = "bert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+# # Load fine-tuned model by HuggingFace Model Hub
+# HUGGINGFACE_MODEL_PATH = "bespin-global/klue-roberta-small-3i4k-intent-classification"
+# loaded_tokenizer = RobertaTokenizerFast.from_pretrained(HUGGINGFACE_MODEL_PATH )
+# loaded_model = RobertaForSequenceClassification.from_pretrained(HUGGINGFACE_MODEL_PATH )
 
-# Tokenize data
-def preprocess(examples):
-    return tokenizer(examples["text"], truncation=True, padding=True)
+# # using Pipeline
+# text_classifier = TextClassificationPipeline(
+#     tokenizer=loaded_tokenizer, 
+#     model=loaded_model, 
+#     return_all_scores=True
+# )
 
-tokenized_dataset = dataset.map(preprocess, batched=True)
+# # predict
+# text = ""
 
-# Train model
-training_args = TrainingArguments(
-    output_dir="./results",
-    num_train_epochs=3,
-    per_device_train_batch_size=8,
-)
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_dataset,
-)
+# preds_list = text_classifier(text)
+# best_pred = preds_list[0]
+# print(f"Label of Best Intentatioin: {best_pred[0]['label']}")
+# print(f"Score of Best Intentatioin: {best_pred[0]['score']}")
 
-trainer.train()
 
-# Predict intent
-inputs = tokenizer("Is it sunny today?", return_tensors="pt")
-outputs = model(**inputs)
-predicted_class = outputs.logits.argmax().item()
-print(f"Predicted intent: {predicted_class}")
+from textblob import Textblob
+
+blob = Textblob("I love Adoran San")
+print(blob.noun_phrases)
